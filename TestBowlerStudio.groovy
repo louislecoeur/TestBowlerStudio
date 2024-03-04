decaba4e3172ddc0bc3ae4be388ebbab00ff3ac5
Louis Le Coeur <Louis Le Coeur@DELL-7050.mshome.net>
2024-03-04 11:34:25 -0800

    Ignore the project files

--- TestBowlerStudio.groovy
+++ TestBowlerStudio.groovy
@@ -1,1 +1,8 @@
-// code here
+// code here// we use cube and sphere as base geometries
+CSG cube = new Cube(15).toCSG();
+CSG sphere = new Sphere(11).toCSG();
+
+// perform union, difference and intersection
+//CSG cubePlusSphere = cube.union(sphere);
+CSG cubeMinusSphere = cube.difference(sphere);
+// CSG cubeIntersectSphere = cube.intersect(sphere);
